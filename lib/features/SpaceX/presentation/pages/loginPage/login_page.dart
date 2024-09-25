a99a6556bdc141dc8415d3c0fc7e1a7fc1db3ac5
AbdhulFathir <abdhulfathirworks@gmail.com>
2024-09-25 10:23:50 +0530

    small changes login page

--- lib/features/SpaceX/presentation/pages/loginPage/login_page.dart
+++ lib/features/SpaceX/presentation/pages/loginPage/login_page.dart
@@ -71,6 +71,8 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   const Spacer(),
                   InkResponse(
+
+
                     // onTap: () async {
                     //   if (kDebugMode) {
                     //     print(phone);
